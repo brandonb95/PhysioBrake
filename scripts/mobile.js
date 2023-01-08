00e0c23ea878dcfb47537a78629096471204e42c
brandonb95 <brandonbirk1995@gmail.com>
2023-01-07 19:23:53 -0800

    Nav Menu updated

--- scripts/mobile.js
+++ scripts/mobile.js
@@ -1,5 +1,19 @@
+//Toggle the mobile menu icon
 $(document).ready(function () {
   $(".hamburger").click(function () {
     $(this).toggleClass("is-active");
   });
 });
+
+btnMenu.addEventListener("click", openMenu);
+// Prevents the focus state from activating
+btnMenu.addEventListener("mousedown", function (e) {
+  e.preventDefault();
+});
+
+// Show mobile menu list
+function openMenu() {
+  // Do some open/close nav stuff here...
+  btnMenu.classList.toggle("active");
+  nav.classList.toggle("active");
+}
